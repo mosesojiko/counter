c168ff019b0866a813053ccee7f28d235e3d310a
Moses Ojiko <mosesojiko999@gmail.com>
2021-09-09 18:20:58 -0700

    Created Product component out of App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,25 @@
 import data from './data';
+import Product from './components/Product'
 
 function App() {
   return (
     <div className="grid-container">
         <header className="row">
             <div>
-                <a className="brand" href="index.html">Mosganda</a>
+                <a className="brand" href="/">Mosganda</a>
             </div>
             <div>
-                <a href="cart.html">Cart</a>
-                <a href="signin.html">Sign In</a>
+                <a href="/store">Store</a>
+                <a href="/cart">Cart</a>
+                <a href="/signup">Sign Up</a>
+                <a href="/signin">Sign In</a>
             </div>
         </header>
         <main>
             <div className="row center">
               {
                 data.products.map((product) =>(
-                  <div key = {product._id} className="card">
-                    <a href="product.html">
-                         
-                    <img className="medium" src = {product.image} alt = {product.name} />
-
-                    </a>
-                    <div className="card-body">
-                        <a href="product.html">
-                            <h2>{product.name}</h2>
-                        </a>
-                        <div className="rating">
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                        </div>
-                        <div className="price">
-                            #{product.price}
-                        </div>
-                    </div>
-                </div>
-                
-
+                  <Product product = {product} />
                 ))
               }
                 
